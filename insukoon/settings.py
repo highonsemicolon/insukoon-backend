c3cc4fd7c4558ec9147fb55ea157a25b0c4bfdc1
onkar <onkar.chendage@gmail.com>
2024-03-06 23:59:07 +0530

    add: referrals app to manage ref data

--- insukoon/settings.py
+++ insukoon/settings.py
@@ -40,9 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "authentication.apps.AuthenticationConfig",
+    'authentication.apps.AuthenticationConfig',
     'rest_framework.authtoken',
-    "profiles.apps.ProfilesConfig",
+    'profiles.apps.ProfilesConfig',
+    'referrals.apps.ReferralsConfig',
 ]
 
 MIDDLEWARE = [
--- insukoon/urls.py
+++ insukoon/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('profile/', include('profiles.urls')),
+    path('refer/', include('referrals.urls'))
 ]
