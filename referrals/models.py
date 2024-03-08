9f393691d66d6c40911ccc01b7f3d704c4e65ce6
onkar <onkar.chendage@gmail.com>
2024-03-09 04:56:34 +0530

    update: referral txn model

--- referrals/models.py
+++ referrals/models.py
@@ -1,18 +1,35 @@
 import random
 import string
+from datetime import datetime, timedelta
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 
 def generate_referral_code():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
 
+def calculate_expiry_date(days):
+    return datetime.now() + timedelta(days=days)
+
+
+class Referrer(models.Model):
+    user_id = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    referral_code = models.CharField(max_length=6, unique=True, default=generate_referral_code)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expiration_date = models.DateTimeField()
+    max_limit = models.IntegerField(default=3)
+    current_count = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.expiration_date = datetime.now() + timedelta(days=30)
+        super().save(*args, **kwargs)
+
+
 class Referral(models.Model):
-    code = models.CharField(max_length=6, unique=True, default=generate_referral_code)
-    expiration_date = models.DateField()
-    max_uses = models.IntegerField(default=3)
-    referrer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='referrals_made')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='referrals')
+    referrer = models.ForeignKey(Referrer, on_delete=models.SET_NULL, null=True, blank=True)
+    Referred_user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
