96a1fb6a31ee3625a8ab16854a0b5416ca0e4a80
onkar <onkar.chendage@gmail.com>
2024-03-15 00:06:27 +0530

    fix: return username on GET profile

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,12 +4,22 @@ from .models import ParentProfile, SchoolProfile
 
 
 class ParentProfileSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
     class Meta:
         model = ParentProfile
         fields = '__all__'
 
+    def get_username(self, obj):
+        return obj.user.username
+
 
 class SchoolProfileSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
     class Meta:
         model = SchoolProfile
         fields = '__all__'
+
+    def get_username(self, obj):
+        return obj.user.username
