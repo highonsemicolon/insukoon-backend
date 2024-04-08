b8d036a8badacb5c18b74e3716729076ac592586
onkar <onkar.chendage@gmail.com>
2024-04-09 01:41:07 +0530

    create: bill

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from authentication.models import CustomUser as User
 
-# Create your models here.
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=8, choices=[('USD', 'Dollars'), ('INR', 'Inr')], default='USD')
+    status = models.CharField(max_length=20,
+                              choices=[('pending', 'Pending'), ('success', 'Success'), ('failed', 'Failed')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    payment_intent_id = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f'Billing information for {self.user.username}'
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from decimal import Decimal
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Payment
+from authentication.models import CustomUser as User
+
+
+class CreateBillView(APIView):
+    def post(self, request):
+        try:
+            amount = None
+            currency = None
+            user_id = request.user.id
+            profile_type = request.user.role
+
+            if profile_type == 'parent':
+                amount = Decimal('9.99')
+            elif profile_type == 'school':
+                amount = Decimal('29.99')
+
+            payment = Payment.objects.create(user_id=user_id, amount=amount, payment_intent_id='temp')
+
+            redirect_url = f"/payment-gateway?amount={amount}&transaction_id={payment.id}"
+            return Response({"redirect_url": redirect_url})
+
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=404)
+
+
+class SubscriptionStatusView(APIView):
+    pass
