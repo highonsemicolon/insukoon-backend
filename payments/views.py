f812050cf53ea1c600a1715847ef34305fb64b5d
onkar <onkar.chendage@gmail.com>
2024-04-09 23:34:33 +0530

    add: referral discount

--- payments/views.py
+++ payments/views.py
@@ -1,25 +1,32 @@
 from decimal import Decimal
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Payment
 from authentication.models import CustomUser as User
+from referrals.models import Transaction
+from .models import Payment
 
 
 class CreateBillView(APIView):
-    def post(self, request):
+    def get(self, request):
         try:
             amount = None
-            currency = None
+            currency = "INR"
             user_id = request.user.id
             profile_type = request.user.role
 
             if profile_type == 'parent':
-                amount = Decimal('9.99')
+                amount = Decimal('10')
             elif profile_type == 'school':
-                amount = Decimal('29.99')
+                amount = Decimal('100')
 
-            payment = Payment.objects.create(user_id=user_id, amount=amount, payment_intent_id='temp')
+            referral = Transaction.objects.filter(referred_user=user_id)
+            if referral.exists():
+                amount = round(amount * Decimal(0.95), 2)
+
+            payment = Payment.objects.create(user_id=user_id, currency=currency, amount=amount,
+                                             payment_intent_id='temp', status='pending')
 
             redirect_url = f"/payment-gateway?amount={amount}&transaction_id={payment.id}"
             return Response({"redirect_url": redirect_url})
